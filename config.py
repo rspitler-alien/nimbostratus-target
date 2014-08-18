938bf9937cfe7d211aa093d89624df52939fafe1
rspitler <rspitler@alienvault.com>
2014-08-18 16:29:05 -0500

    Update RDS security groups

--- config.py
+++ config.py
@@ -1,15 +1,18 @@
 # http://cloud-images.ubuntu.com/locator/ec2/
-REGION = 'ap-southeast-1'
-AMI = 'ami-a02f66f2'
-SIZE = 't1.micro'
+REGION = 'us-east-1'
+AMI = 'ami-b2b061da'
+SIZE = 't2.micro'
 
 
-HTTP_ACCESS_IP = '0.0.0.0'
+DEFAULT_SEC_GROUP = 'default'
+
+
+HTTP_ACCESS_IP = '0.0.0.0/0'
 
 # You will have to fork my repository, configure SSH keys for deploy and
 # then set the paths here. More information can be found here:
 #
 #         https://help.github.com/articles/managing-deploy-keys
 #
-DEPLOY_PRIVATE_PATH = 'keys/github_id_rsa'
-DEPLOY_PUBLIC_PATH = 'keys/github_id_rsa.pub'
+DEPLOY_PRIVATE_PATH = 'keys/id_rsa'
+DEPLOY_PUBLIC_PATH = 'keys/id_rsa.pub'
