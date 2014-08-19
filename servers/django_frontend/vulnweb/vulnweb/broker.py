4df78d6864bf8fc6316097df0d363cec0ac00c4f
rspitler <rspitler@alienvault.com>
2014-08-19 11:38:02 -0500

    update sqs to use us-east-a

--- servers/django_frontend/vulnweb/vulnweb/broker.py
+++ servers/django_frontend/vulnweb/vulnweb/broker.py
@@ -4,5 +4,5 @@ BROKER_URL = '%s://%s:%s@' % (BROKER_TRANSPORT, BOTO_PROVIDER, None)
 CELERY_IGNORE_RESULT = True
 BROKER_TRANSPORT_OPTIONS = {
                             'queue_name_prefix': 'nimbostratus-',
-                            'region': 'ap-southeast-1',
+                            'region': 'us-east-1',
 }
